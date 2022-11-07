b2bcc0d3eb5f47342447ab00106f9be3bba90d84
Julia <juliarusova2014@gmail.com>
2022-11-07 12:22:34 +0800

    Add empty new project

---
+++ Cat finder/Cat finder/Cat_finderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_finderApp.swift
+//  Cat finder
+//
+//  Created by Julia on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Cat_finderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
